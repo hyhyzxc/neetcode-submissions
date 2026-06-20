1d1efc0bf7b580dc2882e213f08476b741a38217
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2026-06-20 23:54:19 +0800

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        done = set()
+
+        for prereq in prerequisites:
+            i, j = prereq[0], prereq[1]
+            adjList[i].append(j)
+        
+        def dfs(i, visited):
+            if i in visited:
+                return False
+            
+            visited.add(i)
+            for neighbor in adjList[i]:
+                if not dfs(neighbor, visited):
+                    return False
+            visited.remove(i)
+
+            return True
+        
+        for i in range(numCourses + 1):
+            res = dfs(i, set())
+            if not res:
+                return False
+                
+        return True
+
+        
