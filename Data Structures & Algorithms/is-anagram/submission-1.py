6f9b6842102ef081457ed8e10f0f942decfa4bee
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2026-06-21 21:29:26 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashMap = defaultdict(int)
+
+        for char in s:
+            hashMap[char] += 1
+        
+        for char in t:
+            if char not in hashMap:
+                return False
+            hashMap[char] -= 1
+            if hashMap[char] == 0:
+                hashMap.pop(char)
+        
+        return True if len(hashMap) == 0 else False
+
+        
