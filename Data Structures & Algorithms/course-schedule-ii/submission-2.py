f0f717f98d75806ed8e0fb02c30bffd2ef269e1a
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2026-06-28 15:12:03 +0800

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjList = defaultdict(list)
+        inDegrees = defaultdict(int)
+
+        for prereq in prerequisites:
+            n1, n2 = prereq[0], prereq[1]
+            adjList[n2].append(n1)
+            inDegrees[n1] += 1
+        
+        queue = deque([])
+        
+        for i in range(numCourses):
+            if inDegrees[i] == 0:
+                queue.append(i)
+        
+        res = []
+        visited = set()
+        while queue:
+            curr = queue.popleft()
+            res.append(curr)
+
+            for neighbour in adjList[curr]:
+                inDegrees[neighbour] -= 1
+                if inDegrees[neighbour] == 0 and neighbour not in visited:
+                    queue.append(neighbour)
+                    
+        
+        return res if len(res) == numCourses else []
